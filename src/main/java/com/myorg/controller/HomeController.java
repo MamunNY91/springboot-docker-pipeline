b28b3b7c16b1036c741bbb3079e292cf37b3d5e0
Mamun <mamun1ny@gmail.com>
2022-11-03 22:17:41 -0400

    initial commit

---
+++ src/main/java/com/myorg/controller/HomeController.java
@@ -0,0 +1,15 @@
+package com.myorg.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController 
+{
+	@GetMapping("/")
+   public String showHomePage() 
+   {
+	   String msg = "hello world";
+	   return msg;
+   }
+}
